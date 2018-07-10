ce70218e97539820c9f32c6703b3c44ec38a1b54
Jovana Ivanovic <jivanovic17@raf.rs>
2018-07-11 00:38:46 +0200

    changed create.blade.php

--- routes/web.php
+++ routes/web.php
@@ -22,11 +22,9 @@ Route::get('/posts/{post}', 'PostController@show');
 Route::post('/posts/{post}/comments', 'CommentsController@store');
 
 
-
 Route::get('/register', 'RegistrationController@create');
 
 Route::post('/register', 'RegistrationController@store');
-
 
 
 Route::get('/login', 'SessionsController@create');
